2b6b414ef8f93644d55bf6dfc729c1990f3b5137
Ekrem <ealp@pgh.yearup.org>
2024-09-25 19:16:33 -0400

    added workbook 1

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println(" 1.Papper");
+        System.out.println(" 2.Banana");
+        System.out.println("3.Apple");
+        System.out.println("4.Watermelon");
+        System.out.println("5.Milk");
+        System.out.println("6. lettuce  ");
+        System.out.println("7.Bread");
+        System.out.println("8. Kiwi");
+        System.out.println("9.Blueberry");
+        System.out.println("10. Bread");
+    }
+}
