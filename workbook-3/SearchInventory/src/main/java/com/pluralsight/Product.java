c0dc291cc3ba24cc6479f30289dfc2156c52ffae
Ekrem <ealp@pgh.yearup.org>
2024-10-13 14:11:12 -0400

    coplated last exercise of 3th week

--- workbook-3/SearchInventory/src/main/java/com/pluralsight/Product.java
+++ workbook-3/SearchInventory/src/main/java/com/pluralsight/Product.java
@@ -15,5 +15,22 @@ public class Product {
             return "Product Code: " + code + ", Name: " + name + ", Price: " + price;
 
         }
+    }package com.pluralsight;
+
+public class Product {
+    private String code;
+    private String name;
+    private double price;
+
+    public Product(String code, String name, double price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
     }
+
+    public String toString() {
+        return "Product Code: " + code + ", Name: " + name + ", Price: " + price;
+
+    }
+}
 
