ff9d54444778703029a983ce8bcf9323a597f6f9
Ekrem <ealp@pgh.yearup.org>
2024-10-31 13:59:51 -0400

    i could do for now i will complated after the class

---
+++ workbook-6/Portfolio/src/main/java/com/pluralsight/finance/BankAccount.java
@@ -0,0 +1,19 @@
+package com.pluralsight.finance;
+
+public class BankAccount implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
+
---
+++ workbook-6/Portfolio/src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -0,0 +1,16 @@
+package com.pluralsight.finance;
+
+public class FixedAsset implements Valuable {
+    private String name;
+    private double value;
+
+    public FixedAsset(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+}
