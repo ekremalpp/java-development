a62bb83950032da43b264df8ec62f3e01c2ed0c1
Ekrem <ealp@pgh.yearup.org>
2024-11-01 12:05:19 -0400

    complated WorkingWithInterfaces exercise

--- workbook-6/WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
+++ workbook-6/WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class Person implements Comparable{
+public class Person implements Comparable<Person>{
     private String firstName;
     private String lastName;
     private int age;
@@ -10,4 +10,38 @@ public class Person implements Comparable{
         this.lastName = lastName;
         this.age = age;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public int compareTo(Person other) {
+
+        int lastNameCompare = this.lastName.compareTo(other.lastName);
+        if (lastNameCompare != 0) {
+            return lastNameCompare;
+        }
+
+        int firstNameCompare = this.firstName.compareTo(other.firstName);
+        if (firstNameCompare != 0) {
+            return firstNameCompare;
+        }
+
+        return Integer.compare(this.age, other.age);
+    }
+
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", Yaş: " + age;
+    }
 }
+
