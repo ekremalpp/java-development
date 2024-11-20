f390a8de3d3b952b28113769528ae322d4fa9be9
Ekrem <ealp@pgh.yearup.org>
2024-11-20 10:20:59 -0500

    added exercise

---
+++ workbook-7/aggregates-and-grouping/sqlscripts.sql
@@ -0,0 +1,42 @@
+-- Question 1 
+SELECT COUNT(*)
+FROM suppliers
+
+
+-- Question 1
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+-- Question 1
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+
+
+-- Question 1
+
+
+
+-- Question 1
