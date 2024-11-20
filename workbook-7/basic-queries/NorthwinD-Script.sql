9384b0c8399adbfab14f95be917eb984605673a3
Ekrem <ealp@pgh.yearup.org>
2024-11-20 07:44:04 -0500

    complated exercise

--- workbook-7/basic-queries/NorthwinD-Script.sql
+++ workbook-7/basic-queries/NorthwinD-Script.sql
@@ -26,17 +26,17 @@ where unitprice <= 7.50;
 -- Question 5:
 SELECT *
 FROM products
-WHERE unitsinstock >=100
-ORDER BY unitprice DESC;
-
+WHERE UnitsInStock <= 100
+ORDER BY UnitPrice DESC
 
 
 
 -- Question 6:
 SELECT *
 FROM products
-WHERE unitsinstock >=100
-ORDER BY unitprice DESC, productname;
+WHERE UnitsInStock <= 100
+ORDER BY UnitPrice DESC , productName
+
 
 
 
@@ -48,35 +48,56 @@ order by ProductName;
 
 
 -- Question 8:
-
-
+--categories
 
 -- Question 9:
-
+SELECT * 
+FROM categories
+-- categoryID 8
 
 
 -- Question 10:
-
+SELECT *
+FROM products
+WHERE categortid = 8
 
 
 
 -- Question 11:
-
+SELECT FirstName, LastName
+FROM Employees;
 
 
 -- Question 12:
+SELECT FirstName, LastName, Title
+FROM Employees
+WHERE Title LIKE "%manager%";
 
 
 
 
 -- Question 13:
-
+SELECT DISTINCT Title 
+FROM Employees;
 
 
 
 
 -- Question 14:
+SELECT FirstName, LastName, Salary
+FROM Employees
+WHERE Salary BETWEEN 2000 and 2500;
 
+
+-- Question 15:
+SELECT * 
+FROM Suppliers;
+
+
+-- Question 16:
+SELECT *
+FROM Products
+WHERE SupplierID =4;
 
 
 
