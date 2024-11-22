86496d5c5f2949945c32935b0b6d4dd2624c84e6
Ekrem <ealp@pgh.yearup.org>
2024-11-21 19:51:25 -0500

    complated  aggregates-and-groupung exercise

--- workbook-7/aggregates-and-grouping/sqlscripts.sql
+++ workbook-7/aggregates-and-grouping/sqlscripts.sql
@@ -1,42 +1,56 @@
 -- Question 1 
 SELECT COUNT(*)
-FROM suppliers
+FROM suppliers;
 
 
--- Question 1
-
-
--- Question 1
-
-
-
--- Question 1
+-- Question 2
+SELECT SUM(salary)
+from employees;
 
 
 
--- Question 1
 
--- Question 1
-
-
--- Question 1
+-- Question 3
+SELECT MIN(unitPrice)
+from products;
 
 
 
--- Question 1
+
+-- Question 4
+SELECT AVG(unitPrice)
+from products;
 
 
 
--- Question 1
+
+-- Question 5
+SELECT MAX(unitPrice)
+from products;
+
+-- Question 6
+SELECT distinct (SupplierID)
+FROM products;
+
+
+-- Question 7
+SELECT categoryID, avg(unitPrice)
+FROM products
+GROUP BY categoryID;
+
+
+-- Question 8
+SELECT supplierId, unitsInStock
+FROM products
+WHERE unitsInStock <= 5;
+
+
+-- Question 9
+SELECT productID,  productName, UnitPrice * UnitsInStock AS InvantoryValue
+FROM products
+ORDER BY InvantoryValue DESC, ProductName ASC;
 
 
 
--- Question 1
 
 
-
--- Question 1
-
-
-
--- Question 1
